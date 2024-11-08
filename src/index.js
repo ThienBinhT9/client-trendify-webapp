55b1923cd77480b74d04aa1cfc8c4e2fbe53426f
ThienBinhT9 <phongtbt903@gmail.comm>
2024-11-08 07:16:56 +0700

    hello trendify

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<></>);
+root.render(
+  <div>
+    <h1>Hello Trendify</h1>
+  </div>
+);
